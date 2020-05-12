ee4887362b9f34107dad439557ded5ee47d87105
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-12 10:28:45 +0800

    Update building policy.

--- app/Policies/BuildingPolicy.php
+++ app/Policies/BuildingPolicy.php
@@ -13,82 +13,82 @@ class BuildingPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\User  $user
+     * @param \App\User $user
      * @return mixed
      */
     public function viewAny(User $user)
     {
-        return true;
+        return $user->hasPermissionTo('View All Buildings');
     }
 
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\User  $user
-     * @param  \App\Building  $building
+     * @param \App\User     $user
+     * @param \App\Building $building
      * @return mixed
      */
     public function view(User $user, Building $building)
     {
-        return $user->isSuperAdmin() || $user->id === $building->manager_id;
+        return $user->hasPermissionTo('View Building') && $user->id === $building->manager_id;
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\User  $user
+     * @param \App\User $user
      * @return mixed
      */
     public function create(User $user)
     {
-        return $user->isSuperAdmin() || $user->isManager();
+        return $user->hasPermissionTo('Create Building');
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\User  $user
-     * @param  \App\Building  $building
+     * @param \App\User     $user
+     * @param \App\Building $building
      * @return mixed
      */
     public function update(User $user, Building $building)
     {
-        return $user->isSuperAdmin() || $user->id == $building->manager_id;
+        return $user->hasPermissionTo('Update Building') && $user->id === $building->manager_id;
     }
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\User  $user
-     * @param  \App\Building  $building
+     * @param \App\User     $user
+     * @param \App\Building $building
      * @return mixed
      */
     public function delete(User $user, Building $building)
     {
-        return  $user->isSuperAdmin() || $user->id == $building->manager_id;
+        return $user->hasPermissionTo('Delete Building');
     }
 
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\User  $user
-     * @param  \App\Building  $building
+     * @param \App\User     $user
+     * @param \App\Building $building
      * @return mixed
      */
     public function restore(User $user, Building $building)
     {
-        //
+        return $user->hasPermissionTo('Restore Building');
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\User  $user
-     * @param  \App\Building  $building
+     * @param \App\User     $user
+     * @param \App\Building $building
      * @return mixed
      */
     public function forceDelete(User $user, Building $building)
     {
-        return $user->isSuperAdmin() || $user->id == $building->manager_id;
+        return $user->hasPermissionTo('Force Delete Building');
     }
 }
