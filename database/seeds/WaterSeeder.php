6c2d840e928b1b4686f9b01eab74c907450763d5
hananloser <hasyrawi@gmail.com>
2020-05-13 07:19:07 +0800

    add factory and seeder

--- database/seeds/WaterSeeder.php
+++ database/seeds/WaterSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Water;
 
 class WaterSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class WaterSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(Water::class, 5)->create();
     }
 }
