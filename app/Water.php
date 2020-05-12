87f2336af08b15e65079b81f933facdd1b073336
hananloser <hasyrawi@gmail.com>
2020-05-13 07:26:26 +0800

    make cast water

--- app/Water.php
+++ app/Water.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Water extends Model
 {
-    //
+    /**
+     * Cast Attributes date to datetime
+     *
+     * @var array
+     */
+    protected $cast = [
+        'date' => 'datetime',
+        'email_verified_at' => 'datetime',
+    ];
+
+
 }
