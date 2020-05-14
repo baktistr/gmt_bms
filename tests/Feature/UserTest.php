ec960ff60dab99871966d5be90071e44007f8bbc
hananloser <hasyrawi@gmail.com>
2020-05-15 05:59:01 +0800

    Example User Test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,21 +2,38 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+    /**
+     * @test
+     *
+     * User Can Added in Database
+     *
+     */
+
+    public function user_can_be_added()
+    {
+        factory(User::class)->create(['name' => 'hanna']);
+        $user = User::where('name', '=', 'hanna')->first();
+        $this->assertEquals('hanna', $user['name']);
+    }
+
+    /**
+     * @test
+     *
+     * user Must exist one in database
+     */
+    public function user_must_exist_one_in_database()
+    {
+
+        factory(User::class)->create();
+        $user =  User::all()->count();
+        $this->assertDatabaseCount('users', 1);
     }
 }
