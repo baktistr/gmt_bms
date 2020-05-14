18a9c07ccab445bd5ca6414c2ecb8e672887bbc4
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-15 07:58:11 +0800

    Use config instead of env.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ env('NOVA_APP_NAME') }}</title>
+    <title>{{ config('nova.name') }}</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
