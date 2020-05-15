176204f7974455210581ce167efd42ef0885c0dc
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-15 10:09:28 +0800

    Configure application version

--- config/app.php
+++ config/app.php
@@ -15,6 +15,11 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    /**
+     * Application version
+     */
+    'version' => env('APP_VERSION'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
