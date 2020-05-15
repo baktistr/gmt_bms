74e116770c057441ae95b23c68d305c9ca196733
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-15 10:17:12 +0800

    Tweaks navigation user template.

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -1,16 +1,22 @@
 <dropdown-trigger class="h-9 flex items-center">
-    @isset($user->email)
-    <img src="https://secure.gravatar.com/avatar/{{ md5(\Illuminate\Support\Str::lower($user->email)) }}?size=512"
-        class="rounded-full w-8 h-8 mr-3" />
-    @endisset
+    <img
+        src="{{ $user->getFirstMediaUrl('avatar', 'medium') }}"
+        class="rounded-full w-8 h-8 mr-3"
+        alt="{{ $user->name }}"
+    />
 
     <span class="text-90">
-        {{ $user->name ?? $user->email ?? __('Nova User') }}
+        {{ $user->name }}
     </span>
 </dropdown-trigger>
 
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
+        <li>
+            <a href="{{ "/nova/resources/users/{$user->id}" }}" class="block no-underline text-90 hover:bg-30 p-3">
+                Profile
+            </a>
+        </li>
         <li>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
                 {{ __('Logout') }}
