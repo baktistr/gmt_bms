e3dfe683d7bb5c41ece89f5316148d6b9f032beb
hananloser <hasyrawi@gmail.com>
2020-05-17 02:11:26 +0800

    seed , migration , factory from csm

---
+++ database/factories/RegencyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Regency;
+use Faker\Generator as Faker;
+
+$factory->define(Regency::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
