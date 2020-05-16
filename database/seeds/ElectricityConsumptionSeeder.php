597d3cb277a568ff30d0a5aba81c6392751d6393
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-16 09:43:59 +0800

    Improve electricity consumption seeder.

--- database/seeds/ElectricityConsumptionSeeder.php
+++ database/seeds/ElectricityConsumptionSeeder.php
@@ -15,10 +15,17 @@ class ElectricityConsumptionSeeder extends Seeder
     {
         // Get the generated buildings from other seeder and seed the electricity consumptions.
         Building::get()->each(function ($building) {
-            for ($i = 1; $i <= 5; $i++) {
+            $electricMeter = 100;
+
+            for ($i = 1; $i <= rand(10, 30); $i++) {
                 factory(ElectricityConsumption::class)->create([
-                    'building_id' => $building->id,
-                    'date'        => now()->addDays($i),
+                    'building_id'    => $building->id,
+                    'date'           => now()->addDays($i),
+                    'electric_meter' => $electricMeter += $usage = rand(100, 500),
+                    'lwbp'           => $lwbp = rand(1, $usage),
+                    'wbp'            => $usage - $lwbp,
+                    'lwbp_rate'      => 1000,
+                    'wbp_rate'       => 2000,
                 ]);
             }
         });
