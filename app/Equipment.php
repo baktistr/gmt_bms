8be04dff8a167943a59072b7211c11e82e17ccc4
hananloser <hasyrawi@gmail.com>
2020-05-28 12:21:14 +0800

    make resource

--- app/Equipment.php
+++ app/Equipment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipment extends Model
 {
-    //
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * Casting year_of_construction to date format
+     *
+     * @var array
+     */
+    protected $casts = [
+        'year_of_construction' => 'date',
+        'date_installation'    => 'date'
+    ];
 }
---
+++ app/Nova/Equipment.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Equipment extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Equipment::class;
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $group = 'Consumptions';
+
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'number',
+        'manufacture'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Number', 'number')
+                ->sortable(),
+
+            Text::make('Manufacture ', 'manufacture')
+                ->sortable(),
+
+            Text::make('Manufacture Number ', 'manufacture_model_number')
+                ->sortable(),
+
+            Date::make('Year Construction', 'year_of_construction')
+                ->sortable(),
+
+
+            Date::make('Date Installation', 'date_installation')
+                ->sortable(),
+
+            Text::make('Cost Center', 'costs_center')
+                ->sortable(),
+
+            Text::make('Location', 'location')
+                ->sortable(),
+
+            Text::make('Barcode Number', 'barcode_number')
+                ->sortable(),
+
+            Text::make('Addtional Information', 'addtional_information')->sortable(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
