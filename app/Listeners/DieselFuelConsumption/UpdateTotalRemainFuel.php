c837a4b14a5c9ce638884ff2198f6fc17517aef7
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-08-28 15:13:43 +0800

    Fix perhitungan sisa pekaian solar

--- app/Listeners/DieselFuelConsumption/UpdateTotalRemainFuel.php
+++ app/Listeners/DieselFuelConsumption/UpdateTotalRemainFuel.php
@@ -32,21 +32,42 @@ class UpdateTotalRemainFuel
              * Dalam hal ini, inputan harus bertipe "incoming". untuk mencegah hasil negatif.
              */
             $totalRemainFuel = $event->dieselFuelConsumption->amount;
+
+            /**
+             * Langsung simpan data awal sisa solar.
+             */
+            $event->dieselFuelConsumption->forceFill(['total_remain' => $totalRemainFuel ?? 0])->save();
         } else {
             /**
              * Jika ada data terakhir, maka hitung sesuai dengan tipe inputan.
              *   - Incoming: total_remain = latest total remain + amount
-             *   - Remain: total_remain = latest total remain - amount
+             *   - Remain: amount = latest total remain - total remain
+             */
+
+            /**
+             * Jika tipenya pengisian, maka jumlahkan sisa solar terakhir
+             * dengan stok solar yang di input (amount)
              */
             if ($event->dieselFuelConsumption->type == 'incoming') {
                 $totalRemainFuel = $latestFuelConsumption->total_remain + $event->dieselFuelConsumption->amount;
+
+                $event->dieselFuelConsumption->forceFill(['total_remain' => $totalRemainFuel ?? 0])->save();
             }
 
+            /**
+             * Jika tipenya pemakaian maka sisa solar adalah "amount" yang di input dari nova,
+             * kemudian "usage (amount)" di update kembali dengan perhitungan sbb:
+             *
+             * (Sisa solar terakhir - sisa solar yang di input (amount))
+             */
             if ($event->dieselFuelConsumption->type == 'remain') {
-                $totalRemainFuel = $latestFuelConsumption->total_remain - $event->dieselFuelConsumption->amount;
+                $totalAmount = $latestFuelConsumption->total_remain - $event->dieselFuelConsumption->amount;
+
+                $event->dieselFuelConsumption->forceFill([
+                    'amount'       => $totalAmount,
+                    'total_remain' => $event->dieselFuelConsumption->amount,
+                ])->save();
             }
         }
-
-        $event->dieselFuelConsumption->forceFill(['total_remain' => $totalRemainFuel ?? 0])->save();
     }
 }
