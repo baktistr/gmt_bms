f2077b5513618bd18df894dd3d35792ba8c8c709
luck_nut <hasyrawi@gmail.com>
2020-09-08 14:42:22 +0800

    fix wrong Namespace

--- app/Nova/BuildingDieselFuelConsumption.php
+++ app/Nova/BuildingDieselFuelConsumption.php
@@ -2,7 +2,7 @@
 
 namespace App\Nova;
 
-use App\Statistik\Statistik;
+use App\Statistics\Statistik;
 use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
 use Epartment\NovaDependencyContainer\NovaDependencyContainer;
 use Illuminate\Http\Request;
